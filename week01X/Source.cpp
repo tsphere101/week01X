6f3d702f9cdc8e11e8e61e1d83c0db6ab12f84c9
Topfee <Topfee@TOPFEELAPTOP>
2020-08-14 20:13:49 +0700

    X

--- week01X/Source.cpp
+++ week01X/Source.cpp
@@ -1,16 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
 int main()
 {
 	int age;
 	char userName[100];
+	printf("\\ \\ if you are older than 18 you are allowed!\n");
 	printf("What is your name? \n");
-	gets_s(userName, 100);
+	gets_s(userName,100);
 
 	printf("How old are you?\n");
-	scanf_s("%d",&age);
+	scanf("%d",&age);
 	if (age > 0) {
 
-		printf("Hello %s!\nYou are %d years old!\n", userName, age);
+		printf("\n\nHello %s!\nYou are %d years old!\n", userName, age);
 
 		if (age >= 18) { printf("You are allowed!\n"); }
 		else if (age < 18) { printf("You are not allowed!\n"); }
